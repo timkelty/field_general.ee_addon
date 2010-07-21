607e95b8da81d683c53dcb81a2fc3a9f51034d6d
Karl Swedberg <karl@englishrules.com>
2010-07-21 15:22:45 -0400

    a few incidental changes

--- themes/field_general_ext/assets/scripts/cp.js
+++ themes/field_general_ext/assets/scripts/cp.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
+  $('table.sortable').children('tbody').each(function(index) {
+    
+  });
+  
   if (window.console && console.log) { console.log('Drag and drop coming soon!');}
 });
